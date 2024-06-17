9124cf6b6ae6dd68d0ff6cee469491eb71345d85
Thomas Barlow <github@mysterymayhem.co.uk>
2024-06-17 19:56:08 +0100

    Simplify goal event location access rules
    Specify only the items required by the location for
    `has_oleander_boss_access()`. The rules only relevant to the region the
    location is in should only belong to the region.
    
    The `final_boss_location` access rule is now constructed from the goal
    locations.

--- worlds/psychonauts/Rules.py
+++ worlds/psychonauts/Rules.py
@@ -413,16 +413,7 @@ class PsyRules:
         return state.has_all([ItemName.LobotoPainting, ItemName.GloriasTrophy, ItemName.StraightJacket], self.player)
 
     def has_oleander_boss_access(self, state: CollectionState) -> bool:
-        items = [
-            ItemName.SashaButton,
-            ItemName.LobotoPainting,
-            ItemName.GloriasTrophy,
-            ItemName.StraightJacket,
-            ItemName.LungfishCall,
-            ItemName.Cake,
-            ItemName.OarsmansBadge
-        ]
-        return state.has_all(items, self.player)
+        return state.has_all([ItemName.Cake, ItemName.Telekinesis, ItemName.Pyrokinesis], self.player)
 
     def redeemed_brain_goal(self, state: CollectionState, amount) -> bool:
         return state.has_group_unique(ItemGroupName.Brains, self.player, amount)
@@ -478,28 +469,24 @@ class PsyRules:
                 add_item_rule(location, forbid_local_only)
 
     def set_psy_goal(self):
-        final_boss_location = self.multiworld.get_location(LocationName.FinalBossEvent, self.player)
         oleander_boss_location = self.multiworld.get_location(LocationName.OleanderBossEvent, self.player)
+        oleander_boss_location.access_rule = self.has_oleander_boss_access
+
         redeemed_required_brains = self.multiworld.get_location(LocationName.RedeemedBrainsEvent, self.player)
+        brains_required = self.world.options.BrainsRequired.value
+        redeemed_required_brains.access_rule = lambda state: self.redeemed_brain_goal(state, brains_required)
+
+        # The Meat Circus Final Boss can only be accessed after completing the other goals.
+        final_boss_location = self.multiworld.get_location(LocationName.FinalBossEvent, self.player)
         # Brain Tank Boss
         if self.world.options.Goal == "braintank":
-            final_boss_location.access_rule = lambda state: self.has_oleander_boss_access(state) and self.has_pyrokinesis(
-                state)
-
+            final_boss_location.access_rule = oleander_boss_location.can_reach
         # Brain Hunt
         elif self.world.options.Goal == "brainhunt":
-            final_boss_location.access_rule = lambda state: self.redeemed_brain_goal(state,
-                                                                                     self.world.options.BrainsRequired.value)
-            redeemed_required_brains.access_rule = lambda state: self.redeemed_brain_goal(state,
-                                                                                          self.world.options.BrainsRequired.value)
-
+            final_boss_location.access_rule = redeemed_required_brains.can_reach
         # Brain Tank Boss AND Brain Hunt
         else:
-            final_boss_location.access_rule = lambda state: self.has_oleander_boss_access(state) and self.has_pyrokinesis(
-                state) and self.redeemed_brain_goal(state, self.world.options.BrainsRequired.value)
-            oleander_boss_location.access_rule = lambda state: self.redeemed_brain_goal(state,
-                                                                                        self.world.options.BrainsRequired.value)
-            redeemed_required_brains.access_rule = lambda state: self.redeemed_brain_goal(state,
-                                                                                          self.world.options.BrainsRequired.value)
+            final_boss_location.access_rule = oleander_boss_location.can_reach
+            add_rule(final_boss_location, redeemed_required_brains.can_reach)
 
         self.multiworld.completion_condition[self.player] = lambda state: state.has(ItemName.Victory, self.player)
