bf5a4654bc6ac10a8d0770ba22a2f741a98eedb2
Thomas Barlow <github@mysterymayhem.co.uk>
2025-07-20 12:07:41 +0100

    Revert "Core: Improve iteration speed of Region.Register objects"
    This got merged into Archipelago 0.6.2, so this commit was duplicating
    the change.
    
    This reverts commit e1b0375addfd64bf4142df731f666804fbddd3d6.

--- BaseClasses.py
+++ BaseClasses.py
@@ -1270,9 +1270,6 @@ class Region:
         def __iter__(self):
             return iter(self._list)
 
-        def __iter__(self):
-            return iter(self._list)
-
         # This seems to not be needed, but that's a bit suspicious.
         # def __del__(self):
         #     self.clear()
