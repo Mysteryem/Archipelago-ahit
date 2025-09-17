25f72bcfd419c2bb4cfd0bf5b477c773eaecb0ed
Classic <ClassicSpeed@users.noreply.github.com>
2025-09-16 21:52:25 -0300

    SADX: Updated to v1.1.3

--- worlds/sadx/__init__.py
+++ worlds/sadx/__init__.py
@@ -15,7 +15,7 @@ from .Regions import create_sadx_regions, get_location_ids_for_area
 from .Rules import create_sadx_rules, LocationDistribution
 from .StartingSetup import StarterSetup, generate_early_sadx, write_sadx_spoiler, CharacterArea, level_areas
 
-sadx_version = 112
+sadx_version = 113
 
 
 class SonicAdventureDXWeb(WebWorld):
