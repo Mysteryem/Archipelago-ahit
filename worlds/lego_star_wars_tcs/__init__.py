1bf131fc51a586a3e91e4178bc5f6eb0ec2a2c5d
Thomas Barlow <github@mysterymayhem.co.uk>
2025-10-16 14:40:11 +0100

    Fix setup tutorial

--- worlds/lego_star_wars_tcs/__init__.py
+++ worlds/lego_star_wars_tcs/__init__.py
@@ -93,7 +93,7 @@ class LegoStarWarsTCSWebWorld(WebWorld):
         "Multiworld Setup Guide",
         "A guide for setting up Lego Star Wars: The Complete Saga to be played in Archipelago.",
         "English",
-        "lsw_tcs_en.md",
+        "setup_en.md",
         "setup/en",
         ["Mysteryem"]
     )]
