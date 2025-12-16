eafb44e45a2181f401337c509ba5613efae7e633
Thomas Barlow <github@mysterymayhem.co.uk>
2025-12-16 19:10:47 +0000

    Fix new Studs items missing form Junk Weights option's valid keys

--- worlds/lego_star_wars_tcs/item_groups.py
+++ worlds/lego_star_wars_tcs/item_groups.py
@@ -38,6 +38,9 @@ ITEM_GROUPS: dict[str, set[str]] = {
     "Minikits": {m.name for m in MINIKITS_BY_COUNT.values() if m.is_sendable},
     "Episode Unlocks": {f"Episode {i} Unlock" for i in "123456"},
     "Junk": {
+        "Silver Stud",
+        "Gold Stud",
+        "Blue Stud",
         "Purple Stud",
         "Power Up",
     },
