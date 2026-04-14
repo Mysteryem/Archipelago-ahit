3018ef36fbfa61a35ef1f3b05ad9424ee13c8f55
Thomas Barlow <github@mysterymayhem.co.uk>
2026-04-14 20:35:32 +0100

    'Fix' Luke Skywalker (Dagobah) not being able to complete Dagobah

--- worlds/lego_star_wars_tcs/client/__init__.py
+++ worlds/lego_star_wars_tcs/client/__init__.py
@@ -55,6 +55,7 @@ from .game_state_modifiers.studs import STUDS_AP_ID_TO_VALUE, give_studs_item
 from .game_state_modifiers.text_display import InGameTextDisplay
 from .game_state_modifiers.text_replacer import TextReplacer
 from .game_state_modifiers.uncap_high_jump import UncapHighJump
+from .game_state_modifiers.luke_dagobah_fix import LukeDagobahFix
 
 
 # Universal Tracker client integration.
@@ -422,11 +423,13 @@ class LegoStarWarsTheCompleteSagaContext(CommonContext):
         self.locked_cantina_door_info = LockedCantinaDoorDisplay()
         self.uncap_high_jump = UncapHighJump()
         self.cantina_reloader = CantinaReloader()
+        self.luke_dagobah_fix = LukeDagobahFix()
         self.permanent_components = (
             self.text_display,
             self.uncap_high_jump,
             self.cantina_reloader,
             self.locked_cantina_door_info,
+            self.luke_dagobah_fix,
         )
 
         self.death_link_manager = DeathLinkManager()
