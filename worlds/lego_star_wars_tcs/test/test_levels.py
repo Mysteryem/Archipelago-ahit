5b479899f253f8121221c667372c6dbc4cb4abc1
Thomas Barlow <github@mysterymayhem.co.uk>
2026-05-11 20:28:20 +0100

    Add playable level IDs to Area data

--- worlds/lego_star_wars_tcs/test/test_levels.py
+++ worlds/lego_star_wars_tcs/test/test_levels.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 from ..constants import CharacterAbility
 from ..items import ITEM_DATA_BY_NAME, CHARACTERS_AND_VEHICLES_BY_NAME
-from ..levels import CHAPTER_AREAS
+from ..levels import CHAPTER_AREAS, BONUS_AREAS
 from ..locations import LOCATION_NAME_TO_ID
 
 
@@ -28,3 +28,19 @@ class TestLevels(TestCase):
                 for character in area.character_requirements:
                     story_character_abilities |= CHARACTERS_AND_VEHICLES_BY_NAME[character].abilities
                 self.assertLessEqual(area.completion_main_ability_requirements, story_character_abilities)
+
+    def test_playable_level_ids(self):
+        all_ids_so_far = set()
+        for areas in [CHAPTER_AREAS, BONUS_AREAS]:
+            for area in areas:
+                with self.subTest(area.name):
+                    level_ids = area.playable_level_ids
+                    self.assertEqual(len(level_ids), len(set(level_ids)))
+                    self.assertEqual(list(level_ids), sorted(level_ids))
+                    if level_ids:
+                        previous = level_ids[0]
+                        for level_id in level_ids[1:]:
+                            self.assertEqual(previous + 1, level_id)
+                            previous = level_id
+                    self.assertTrue(all_ids_so_far.isdisjoint(level_ids))
+                    all_ids_so_far.update(level_ids)
