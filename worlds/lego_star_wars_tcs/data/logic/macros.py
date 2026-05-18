dc925a022734b10d413d1371b1c3676b6387de1c
Thomas Barlow <github@mysterymayhem.co.uk>
2026-05-18 23:09:20 +0100

    Update rule macros

--- worlds/lego_star_wars_tcs/data/logic/macros.py
+++ worlds/lego_star_wars_tcs/data/logic/macros.py
@@ -1,4 +1,4 @@
-from rule_builder.rules import Has, HasAny, Filtered, True_, Or, And
+from rule_builder.rules import Has, HasAny, True_, Or, And
 
 from .option_filters import normal_logic, logic_options
 from .rules import HasAbility, HasAnyAbilities, HasAllAbilities
@@ -27,16 +27,36 @@ can_destroy_close_silver_bricks = Or(
         Has("Super Ewok Catapult") & HasAbility(WEAPON_EWOK),
     )
 )
-can_damage_shielded_droideka = Or(
+# Only expect Jedi, Bounty Hunter or Droideka.
+base_can_damage_shielded_droideka = Or(
     HasAnyAbilities(JEDI | BOUNTY_HUNTER),
     Has("Droideka"),
-    HasAllAbilities(WEAPON_ZAPPER | CAN_ATTACK_UP_CLOSE),
-    Or(
-        HasAbility(CAN_SELF_DESTRUCT) & Has("Self Destruct"),
-        Has("Exploding Blaster Bolts") & HasAnyAbilities(BLASTER | WEAPON_EWOK),
-        Has("Super Ewok Catapult") & HasAbility(WEAPON_EWOK),
-        Has("Deflect Bolts"),
-    )
+)
+can_damage_shielded_droideka = logic_options(
+    base=base_can_damage_shielded_droideka,
+    # Adds zappers and Extras.
+    normal=Or(
+        HasAnyAbilities(JEDI | BOUNTY_HUNTER),
+        Has("Droideka"),
+        HasAllAbilities(WEAPON_ZAPPER | CAN_ATTACK_UP_CLOSE),
+        Or(
+            HasAbility(CAN_SELF_DESTRUCT) & Has("Self Destruct"),
+            Has("Exploding Blaster Bolts") & HasAnyAbilities(BLASTER | WEAPON_EWOK),
+            Has("Super Ewok Catapult") & HasAbility(WEAPON_EWOK),
+        )
+    ),
+    # Adds Deflect Bolts Extra.
+    moderate=Or(
+        HasAnyAbilities(JEDI | BOUNTY_HUNTER),
+        Has("Droideka"),
+        HasAllAbilities(WEAPON_ZAPPER | CAN_ATTACK_UP_CLOSE),
+        Or(
+            HasAbility(CAN_SELF_DESTRUCT) & Has("Self Destruct"),
+            Has("Exploding Blaster Bolts") & HasAnyAbilities(BLASTER | WEAPON_EWOK),
+            Has("Super Ewok Catapult") & HasAbility(WEAPON_EWOK),
+            Has("Deflect Bolts"),
+        )
+    ),
 )
 can_destroy_far_silver_bricks = Or(
     HasAbility(BOUNTY_HUNTER),
@@ -54,7 +74,7 @@ can_fight_or_bypass_skippable_droideka = Or(
 )
 can_damage_at_close_range = Or(
     HasAbility(CAN_ATTACK_UP_CLOSE),
-    Filtered(can_self_destruct, options=normal_logic)
+    can_self_destruct
 )
 
 can_activate_close_target = logic_options(
