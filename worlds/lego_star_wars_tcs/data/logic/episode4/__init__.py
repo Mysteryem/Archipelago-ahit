6b2deee2e8d452ac44902600454c3df9366c1ec3
Thomas Barlow <github@mysterymayhem.co.uk>
2026-06-02 02:19:12 +0100

    Add legacy 4-5 logic

--- worlds/lego_star_wars_tcs/data/logic/episode4/__init__.py
+++ worlds/lego_star_wars_tcs/data/logic/episode4/__init__.py
@@ -2,6 +2,7 @@ from .chapter1 import SECRET_PLANS
 from .chapter2 import THROUGH_THE_JUNDLAND_WASTES
 from .chapter3 import MOS_EISLEY_SPACEPORT
 from .chapter4 import RESCUE_THE_PRINCESS
+from .chapter5 import DEATH_STAR_ESCAPE
 
 __all__ = [
     "CHAPTERS",
@@ -12,4 +13,5 @@ CHAPTERS = (
     THROUGH_THE_JUNDLAND_WASTES,
     MOS_EISLEY_SPACEPORT,
     RESCUE_THE_PRINCESS,
+    DEATH_STAR_ESCAPE,
 )
---
+++ worlds/lego_star_wars_tcs/data/logic/episode4/chapter5.py
@@ -0,0 +1,22 @@
+from ..types import make_legacy_chapter, LegacyMinikitData
+
+DEATH_STAR_ESCAPE = make_legacy_chapter(
+    short_name="4-5",
+    start_level="deathstarescape_a",
+    minikits={
+        "Minikit Behind Silver Bricks": LegacyMinikitData.new("deathstarescape_a", "m_pup1"),
+        "Access Hatch Minikit": LegacyMinikitData.new("deathstarescape_a", "mk_0"),
+        "Window Washing Minikit": LegacyMinikitData.new("deathstarescape_b", "pup1"),
+        "Minikit Behind Sith Force Bricks": LegacyMinikitData.new("deathstarescape_b", "pup2"),
+        "High Minikit In Cylindrical Room": LegacyMinikitData.new("deathstarescape_b", "pup3"),
+        "Hangar Right Minikit": LegacyMinikitData.new("deathstarescape_c", "pup1"),
+        "Minikit By Vader": LegacyMinikitData.new("deathstarescape_c", "pup2"),
+        "Stormtrooper Reinforcements Room Minikit": LegacyMinikitData.new("deathstarescape_c", "pup3"),
+        "Corridor Start Minikit": LegacyMinikitData.new("deathstarescape_c", "pup4"),
+        "Reveal Three Berry Tiles Minikit": LegacyMinikitData.new("deathstarescape_c", "m_pup1"),
+    },
+    extra_toggle_characters=(
+        "Mouse Droid",
+        "Imperial Engineer",
+    )
+)
