e61bb2bee834bc699cb2a2e43c4047a44801edd2
Thomas Barlow <github@mysterymayhem.co.uk>
2026-06-04 02:55:17 +0100

    Add Episode 5 and 6 to the Episodes list
    Forgot to update this when adding logic for episode 5 and 6 chapters

--- worlds/lego_star_wars_tcs/data/logic/__init__.py
+++ worlds/lego_star_wars_tcs/data/logic/__init__.py
@@ -2,6 +2,8 @@ from .episode1 import CHAPTERS as EP1_CHAPTERS
 from .episode2 import CHAPTERS as EP2_CHAPTERS
 from .episode3 import CHAPTERS as EP3_CHAPTERS
 from .episode4 import CHAPTERS as EP4_CHAPTERS
+from .episode5 import CHAPTERS as EP5_CHAPTERS
+from .episode6 import CHAPTERS as EP6_CHAPTERS
 from .types import Chapter
 
 __all__ = [
@@ -13,6 +15,8 @@ EPISODES = (
     EP2_CHAPTERS,
     EP3_CHAPTERS,
     EP4_CHAPTERS,
+    EP5_CHAPTERS,
+    EP6_CHAPTERS,
 )
 
 CHAPTERS_BY_SHORT_NAME: dict[str, Chapter] = {
