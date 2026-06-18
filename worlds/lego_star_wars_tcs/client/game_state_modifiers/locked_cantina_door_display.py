239b711af6acabf1c0bdfc00235934b280c86685
Thomas Barlow <github@mysterymayhem.co.uk>
2026-06-19 00:00:17 +0100

    Client: locked_cantina_door_display.py: add underscores to private module globals

--- worlds/lego_star_wars_tcs/client/game_state_modifiers/locked_cantina_door_display.py
+++ worlds/lego_star_wars_tcs/client/game_state_modifiers/locked_cantina_door_display.py
@@ -13,36 +13,36 @@ from ...data.levels import Level
 
 
 # Technically this is the feet position
-CHARACTER_POSITION_VECTOR = NuVecField(0x5c)
+_CHARACTER_POSITION_VECTOR = NuVecField(0x5c)
 """Feet position of a character (GameObject_s)."""
 
-HUB_EPISODE_TIME = StaticFloat(0x879594)
+_HUB_EPISODE_TIME = StaticFloat(0x879594)
 """Timer float for the Cantina episode door on-screen text. If this is set by the game, then the on-screen text for
 an episode door is currently being displayed. Setting this above zero do *not* cause the on-screen text to display."""
 
-HUB_AREA_TIME = StaticFloat(0x879528)
+_HUB_AREA_TIME = StaticFloat(0x879528)
 """
 Timer float for the Cantina area door on-screen text. Setting this above zero causes the on-screen text to display
-according to the Area ID that the value at HUB_AREA is set to.
+according to the Area ID that the value at _HUB_AREA is set to.
 """
 
-HUB_AREA = StaticInt(0x879b20)
+_HUB_AREA = StaticInt(0x879b20)
 """Area ID for the Cantina level door on-screen text."""
 
-A_DATA_LIST_PTR = StaticUint(0x951374)
+_A_DATA_LIST_PTR = StaticUint(0x951374)
 """Pointer to the start of the AREADATA_s array _ADataList."""
 
-AREA_DATA_SIZE = 0x9c
+_AREA_DATA_SIZE = 0x9c
 """sizeof(AREADATA_s)"""
 
-AREA_DATA_NAME_ID = UShortField(0x78)
+_AREA_DATA_NAME_ID = UShortField(0x78)
 """.NameId of AREADATA_s, the localisation text ID of the area."""
 
-AREA_TO_USE = Area.E1ENDING
+_AREA_TO_USE = Area.E1ENDING
 
 
 @dataclass(frozen=True)
-class Coordinates:
+class _Coordinates:
     x: float
     y: float
     z: float
@@ -68,92 +68,92 @@ class Coordinates:
     #     return (self.x - other.x) ** 2 + (self.z - other.z) ** 2
 
 
-ACTIVATION_DISTANCE = 0.30734664296783093
-# EPISODE_1_DOOR_ACTIVATION_FURTHEST_ACTIVATION = Coordinates(-23.21241951, 0.001831591129, -51.91680908)
+_ACTIVATION_DISTANCE = 0.30734664296783093
+# EPISODE_1_DOOR_ACTIVATION_FURTHEST_ACTIVATION = _Coordinates(-23.21241951, 0.001831591129, -51.91680908)
 
 # These coordinates were recorded by standing against the locked door, roughly in the middle.
 # The bonus doors were already open on the file I used, so I stood within the doorframe instead.
-MAIN_ROOM_DOORS = {
-    "1": Coordinates(-22.9132, 0.0013, -51.9870),
-    "2": Coordinates(-23.2557, 0.0013, -52.9901),
-    "3": Coordinates(-23.7055, 0.0013, -53.9667),
-    "4": Coordinates(-24.3302, 0.0013, -54.8765),
-    "5": Coordinates(-25.2044, 0.0014, -55.5948),
-    "6": Coordinates(-26.2368, 0.0014, -56.1084),
-    # "arcade": Coordinates(-22.4908, 0.0022, -49.889),
-    # "bonus": Coordinates(-28.2893, 0.0015, -57.1381),
-    # "junkyard": Coordinates(-27.9784, 0.1109, -46.8383),
+_MAIN_ROOM_DOORS = {
+    "1": _Coordinates(-22.9132, 0.0013, -51.9870),
+    "2": _Coordinates(-23.2557, 0.0013, -52.9901),
+    "3": _Coordinates(-23.7055, 0.0013, -53.9667),
+    "4": _Coordinates(-24.3302, 0.0013, -54.8765),
+    "5": _Coordinates(-25.2044, 0.0014, -55.5948),
+    "6": _Coordinates(-26.2368, 0.0014, -56.1084),
+    # "arcade": _Coordinates(-22.4908, 0.0022, -49.889),
+    # "bonus": _Coordinates(-28.2893, 0.0015, -57.1381),
+    # "junkyard": _Coordinates(-27.9784, 0.1109, -46.8383),
 }
-EPISODE_1_DOORS = {
-    "1": Coordinates(-13.7371, 0.0013, -55.6437),
-    "2": Coordinates(-13.5916, 0.0013, -54.0831),
-    "3": Coordinates(-12.8858, 0.0013, -52.9075),
-    "4": Coordinates(-11.6144, 0.0013, -52.2382),
-    "5": Coordinates(-9.9517, 0.0013, -51.9606),
-    "6": Coordinates(-8.4534, 0.0013, -52.4315),
-    # "bonus": Coordinates(-6.4901, 0.0015, -53.8392),
+_EPISODE_1_DOORS = {
+    "1": _Coordinates(-13.7371, 0.0013, -55.6437),
+    "2": _Coordinates(-13.5916, 0.0013, -54.0831),
+    "3": _Coordinates(-12.8858, 0.0013, -52.9075),
+    "4": _Coordinates(-11.6144, 0.0013, -52.2382),
+    "5": _Coordinates(-9.9517, 0.0013, -51.9606),
+    "6": _Coordinates(-8.4534, 0.0013, -52.4315),
+    # "bonus": _Coordinates(-6.4901, 0.0015, -53.8392),
 }
-EPISODE_2_DOORS = {
-    "1": Coordinates(-12.2607, 0.0013, -64.5745),
-    "2": Coordinates(-10.8183, 0.0013, -64.5931),
-    "3": Coordinates(-9.4126, 0.0013, -64.3750),
-    "4": Coordinates(-7.9266, 0.0013, -63.7392),
-    "5": Coordinates(-6.7660, 0.0013, -64.6734),
-    "6": Coordinates(-6.6051, 0.0013, -66.0659),
-    # "bonus": Coordinates(-5.7765, 0.0015, -68.8038),
+_EPISODE_2_DOORS = {
+    "1": _Coordinates(-12.2607, 0.0013, -64.5745),
+    "2": _Coordinates(-10.8183, 0.0013, -64.5931),
+    "3": _Coordinates(-9.4126, 0.0013, -64.3750),
+    "4": _Coordinates(-7.9266, 0.0013, -63.7392),
+    "5": _Coordinates(-6.7660, 0.0013, -64.6734),
+    "6": _Coordinates(-6.6051, 0.0013, -66.0659),
+    # "bonus": _Coordinates(-5.7765, 0.0015, -68.8038),
 }
-EPISODE_3_DOORS = {
-    "1": Coordinates(-7.4674, 0.0013, -74.3870),
-    "2": Coordinates(-7.4676, 0.0013, -75.5595),
-    "3": Coordinates(-7.3477, 0.0013, -76.6879),
-    "4": Coordinates(-7.1177, 0.0013, -78.0387),
-    "5": Coordinates(-7.2158, 0.0013, -79.3340),
-    "6": Coordinates(-7.8197, 0.0013, -80.4848),
-    # "bonus": Coordinates(-10.0878, 0.0013, -81.1680),
+_EPISODE_3_DOORS = {
+    "1": _Coordinates(-7.4674, 0.0013, -74.3870),
+    "2": _Coordinates(-7.4676, 0.0013, -75.5595),
+    "3": _Coordinates(-7.3477, 0.0013, -76.6879),
+    "4": _Coordinates(-7.1177, 0.0013, -78.0387),
+    "5": _Coordinates(-7.2158, 0.0013, -79.3340),
+    "6": _Coordinates(-7.8197, 0.0013, -80.4848),
+    # "bonus": _Coordinates(-10.0878, 0.0013, -81.1680),
 }
-EPISODE_4_DOORS = {
-    "1": Coordinates(-11.9416, 0.0013, -90.8385),
-    "2": Coordinates(-10.8066, 0.0013, -90.8385),
-    "3": Coordinates(-9.6827, 0.0013, -90.9004),
-    "4": Coordinates(-7.1865, 0.0013, -93.2124),
-    "5": Coordinates(-7.1753, 0.0013, -94.3230),
-    "6": Coordinates(-7.1465, 0.0013, -95.4684),
-    # "bonus": Coordinates(-7.0653, 0.0013, -98.1796),
+_EPISODE_4_DOORS = {
+    "1": _Coordinates(-11.9416, 0.0013, -90.8385),
+    "2": _Coordinates(-10.8066, 0.0013, -90.8385),
+    "3": _Coordinates(-9.6827, 0.0013, -90.9004),
+    "4": _Coordinates(-7.1865, 0.0013, -93.2124),
+    "5": _Coordinates(-7.1753, 0.0013, -94.3230),
+    "6": _Coordinates(-7.1465, 0.0013, -95.4684),
+    # "bonus": _Coordinates(-7.0653, 0.0013, -98.1796),
 }
-EPISODE_5_DOORS = {
-    "1": Coordinates(-13.3404, 0.0013, -108.3106),
-    "2": Coordinates(-12.3556, 0.0013, -106.8501),
-    "3": Coordinates(-10.7851, 0.0013, -106.1567),
-    "4": Coordinates(-9.4099, 0.0013, -105.9008),
-    "5": Coordinates(-7.8177, 0.0013, -106.4374),
-    "6": Coordinates(-7.0013, 0.0013, -107.5948),
-    # "bonus": Coordinates(-5.7996, 0.0013, -110.5100),
+_EPISODE_5_DOORS = {
+    "1": _Coordinates(-13.3404, 0.0013, -108.3106),
+    "2": _Coordinates(-12.3556, 0.0013, -106.8501),
+    "3": _Coordinates(-10.7851, 0.0013, -106.1567),
+    "4": _Coordinates(-9.4099, 0.0013, -105.9008),
+    "5": _Coordinates(-7.8177, 0.0013, -106.4374),
+    "6": _Coordinates(-7.0013, 0.0013, -107.5948),
+    # "bonus": _Coordinates(-5.7996, 0.0013, -110.5100),
 }
-EPISODE_6_DOORS = {
-    "1": Coordinates(-10.2091, 0.0013, -120.9750),
-    "2": Coordinates(-9.2976, 0.0013, -120.0989),
-    "3": Coordinates(-8.1642, 0.0013, -119.6590),
-    "4": Coordinates(-6.9686, 0.0013, -119.8224),
-    "5": Coordinates(-5.9117, 0.0013, -120.4271),
-    "6": Coordinates(-5.1751, 0.0013, -121.4412),
-    # "bonus": Coordinates(-4.1028, 0.0015, -125.8803),
+_EPISODE_6_DOORS = {
+    "1": _Coordinates(-10.2091, 0.0013, -120.9750),
+    "2": _Coordinates(-9.2976, 0.0013, -120.0989),
+    "3": _Coordinates(-8.1642, 0.0013, -119.6590),
+    "4": _Coordinates(-6.9686, 0.0013, -119.8224),
+    "5": _Coordinates(-5.9117, 0.0013, -120.4271),
+    "6": _Coordinates(-5.1751, 0.0013, -121.4412),
+    # "bonus": _Coordinates(-4.1028, 0.0015, -125.8803),
 }
 # JUNKYARD_DOORS = {
-#     "cantina": Coordinates(-30.6921, 0.04027, -35.0349),
-#     "missions": Coordinates(-33.2018, -0.0048, -29.2741),
+#     "cantina": _Coordinates(-30.6921, 0.04027, -35.0349),
+#     "missions": _Coordinates(-33.2018, -0.0048, -29.2741),
 # }
 # MISSIONS_DOORS = {
-#     "junkyard": Coordinates(-31.8879, 0.1481, -72.2756),
+#     "junkyard": _Coordinates(-31.8879, 0.1481, -72.2756),
 # }
 
-RELEVANT_ROOMS_TO_DOORS: dict[CantinaRoom, dict[str, Coordinates]] = {
-    CantinaRoom.SHOP_ROOM: MAIN_ROOM_DOORS,
-    CantinaRoom.EPISODE_1: EPISODE_1_DOORS,
-    CantinaRoom.EPISODE_2: EPISODE_2_DOORS,
-    CantinaRoom.EPISODE_3: EPISODE_3_DOORS,
-    CantinaRoom.EPISODE_4: EPISODE_4_DOORS,
-    CantinaRoom.EPISODE_5: EPISODE_5_DOORS,
-    CantinaRoom.EPISODE_6: EPISODE_6_DOORS,
+_RELEVANT_ROOMS_TO_DOORS: dict[CantinaRoom, dict[str, _Coordinates]] = {
+    CantinaRoom.SHOP_ROOM: _MAIN_ROOM_DOORS,
+    CantinaRoom.EPISODE_1: _EPISODE_1_DOORS,
+    CantinaRoom.EPISODE_2: _EPISODE_2_DOORS,
+    CantinaRoom.EPISODE_3: _EPISODE_3_DOORS,
+    CantinaRoom.EPISODE_4: _EPISODE_4_DOORS,
+    CantinaRoom.EPISODE_5: _EPISODE_5_DOORS,
+    CantinaRoom.EPISODE_6: _EPISODE_6_DOORS,
 }
 
 
@@ -171,9 +171,9 @@ class LockedCantinaDoorDisplay(ClientComponent):
         # The Episode 1 Ending Area is picked because Episode endings are not levels that can be accessed directly from
         # the Cantina, so do not have a localised Text ID assigned to their name in vanilla, meaning it can be freely
         # overridden as needed.
-        a_data_list_addr = A_DATA_LIST_PTR.get(event.context)
-        episode_1_ending_area_data_addr = a_data_list_addr + AREA_DATA_SIZE * AREA_TO_USE
-        AREA_DATA_NAME_ID.set(event.context, episode_1_ending_area_data_addr, TextId.WII_MOTION_CONTROL_HINT_1)
+        a_data_list_addr = _A_DATA_LIST_PTR.get(event.context)
+        episode_1_ending_area_data_addr = a_data_list_addr + _AREA_DATA_SIZE * _AREA_TO_USE
+        _AREA_DATA_NAME_ID.set(event.context, episode_1_ending_area_data_addr, TextId.WII_MOTION_CONTROL_HINT_1)
 
         # Initialise active state based on the current level ID.
         self.active = event.context.current_level_id == Level.MAP
@@ -248,9 +248,9 @@ class LockedCantinaDoorDisplay(ClientComponent):
                 has_drawn = False
         if has_drawn:
             # Set the area ID used by the game to display
-            HUB_AREA.set(ctx, AREA_TO_USE)
+            _HUB_AREA.set(ctx, _AREA_TO_USE)
             # Set/refresh the timer.
-            HUB_AREA_TIME.set(ctx, 1.1)
+            _HUB_AREA_TIME.set(ctx, 1.1)
             self.is_drawing = True
 
     async def _draw_locked_door_info(self, event: OnGameWatcherTickEvent):
@@ -262,28 +262,28 @@ class LockedCantinaDoorDisplay(ClientComponent):
         self.is_drawing = False
         ctx = event.context
         current_room = ctx.current_cantina_room
-        if current_room not in RELEVANT_ROOMS_TO_DOORS:
+        if current_room not in _RELEVANT_ROOMS_TO_DOORS:
             # The player is in a room we don't care about.
             return
 
-        current_doors = RELEVANT_ROOMS_TO_DOORS[current_room]
+        current_doors = _RELEVANT_ROOMS_TO_DOORS[current_room]
 
-        if HUB_EPISODE_TIME.get(ctx) > 0:
+        if _HUB_EPISODE_TIME.get(ctx) > 0:
             # Episode door text is currently on-screen.
             if was_drawing:
                 # Ensure the Area text is hidden if Episode text is currently being displayed. They overlap otherwise.
-                HUB_AREA_TIME.set(ctx, 0.0)
+                _HUB_AREA_TIME.set(ctx, 0.0)
             return
 
-        if HUB_AREA.get(ctx) not in (-1, AREA_TO_USE):
+        if _HUB_AREA.get(ctx) not in (-1, _AREA_TO_USE):
             # Area door text other than our own is currently on-screen.
             return
 
-        # Find if a player is within ACTIVATION_DISTANCE of a relevant door within this room.
+        # Find if a player is within _ACTIVATION_DISTANCE of a relevant door within this room.
         for _player_number, player_address in player_character_entity_iter(ctx):
-            player_pos = Coordinates(*CHARACTER_POSITION_VECTOR.get(ctx, player_address))
+            player_pos = _Coordinates(*_CHARACTER_POSITION_VECTOR.get(ctx, player_address))
             for door_name, door_pos in current_doors.items():
-                if player_pos.distance(door_pos) < ACTIVATION_DISTANCE:
+                if player_pos.distance(door_pos) < _ACTIVATION_DISTANCE:
                     self._draw_info(ctx, current_room, door_name)
                     return
 
--- worlds/lego_star_wars_tcs/test/test_door_info.py
+++ worlds/lego_star_wars_tcs/test/test_door_info.py
@@ -4,24 +4,25 @@ from unittest import TestCase
 
 from ..client.game_state_modifiers import locked_cantina_door_display as door_display
 
-ALL_DOORS: list[Iterable[door_display.Coordinates]] = [
-    door_display.MAIN_ROOM_DOORS.values(),
-    door_display.EPISODE_1_DOORS.values(),
-    door_display.EPISODE_2_DOORS.values(),
-    door_display.EPISODE_3_DOORS.values(),
-    door_display.EPISODE_4_DOORS.values(),
-    door_display.EPISODE_5_DOORS.values(),
-    door_display.EPISODE_6_DOORS.values(),
+# noinspection PyProtectedMember
+ALL_DOORS: list[Iterable[door_display._Coordinates]] = [
+    door_display._MAIN_ROOM_DOORS.values(),
+    door_display._EPISODE_1_DOORS.values(),
+    door_display._EPISODE_2_DOORS.values(),
+    door_display._EPISODE_3_DOORS.values(),
+    door_display._EPISODE_4_DOORS.values(),
+    door_display._EPISODE_5_DOORS.values(),
+    door_display._EPISODE_6_DOORS.values(),
 ]
 
 
 class TestDoorInfo(TestCase):
-    MIN_DOOR_DISTANCE = door_display.ACTIVATION_DISTANCE
+    MIN_DOOR_DISTANCE = door_display._ACTIVATION_DISTANCE
 
     def test_distances_between_doors(self):
-        for doors_dict in door_display.RELEVANT_ROOMS_TO_DOORS.values():
-            door1: door_display.Coordinates
-            door2: door_display.Coordinates
+        for doors_dict in door_display._RELEVANT_ROOMS_TO_DOORS.values():
+            door1: door_display._Coordinates
+            door2: door_display._Coordinates
             for door1, door2 in itertools.combinations(doors_dict.values(), r=2):
                 # No overlap.
                 self.assertGreater(door1.xz_distance(door2), self.MIN_DOOR_DISTANCE * 2)
