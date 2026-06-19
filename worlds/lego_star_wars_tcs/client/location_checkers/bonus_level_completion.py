04b0054a8b0c0ccaa41e1d47ff8a4d8ea1ca0dad
Thomas Barlow <github@mysterymayhem.co.uk>
2026-06-19 17:52:33 +0100

    Client: update bonus_level_completion.py
    Also added underscores to private module globals

--- worlds/lego_star_wars_tcs/client/location_checkers/bonus_level_completion.py
+++ worlds/lego_star_wars_tcs/client/location_checkers/bonus_level_completion.py
@@ -1,20 +1,31 @@
 import logging
 from typing import Iterable
 
-from ..common import ClientComponent
+from ..common import ClientComponent, UCharField, StaticUChar
 from ..events import subscribe_event, OnReceiveSlotDataEvent
 from ..type_aliases import MemoryAddress, ApLocationId, TCSContext, AreaId
-from ...levels import BONUS_AREAS, BONUS_NAME_TO_BONUS_AREA
 from ...locations import LOCATION_NAME_TO_ID
 
+from ...data.areas import Area, BONUS_ROOM_BONUSES
 
-debug_logger = logging.getLogger("TCS Debug")
+
+_DEBUG_LOGGER = logging.getLogger("TCS Debug")
 
 
-ALL_STORY_COMPLETION_CHECKS: dict[AreaId, tuple[ApLocationId, MemoryAddress]] = {
-    bonus.area_id: (LOCATION_NAME_TO_ID[bonus.completion_location_name], bonus.address + bonus.completion_offset)
-    for bonus in BONUS_AREAS
+_AREA_COMPLETION = UCharField(0x1)
+_INDY_TRAILER_WATCHED = StaticUChar(0x86e505)
+
+
+_ALL_STORY_COMPLETION_CHECKS: dict[Area, tuple[ApLocationId, MemoryAddress]] = {
+    area: (LOCATION_NAME_TO_ID[area.get_completion_name()], area.save_data_address + _AREA_COMPLETION)
+    for area in BONUS_ROOM_BONUSES
 }
+# The Indiana Jones trailer has its own part of memory that it writes to, to mark that it has been watched, rather than
+# marking the Lost Temple Area's save-data as completed.
+_ALL_STORY_COMPLETION_CHECKS[Area.LOSTTEMPLE] = (
+    LOCATION_NAME_TO_ID[Area.LOSTTEMPLE.get_completion_name()],
+    _INDY_TRAILER_WATCHED,
+)
 
 
 class BonusAreaCompletionChecker(ClientComponent):
@@ -24,11 +35,11 @@ class BonusAreaCompletionChecker(ClientComponent):
     """
     # Anakin's Flight and A New Hope support Free Play, but the rest are Story mode only, for now, this class only
     # checks for story completion.
-    remaining_story_completion_checks: dict[AreaId, tuple[ApLocationId, MemoryAddress]]
-    remaining_bonus_area_completion_locations: dict[AreaId, list[ApLocationId]]
+    remaining_story_completion_checks: dict[Area, tuple[ApLocationId, MemoryAddress]]
+    remaining_bonus_area_completion_locations: dict[Area, list[ApLocationId]]
 
     def __init__(self):
-        self.remaining_story_completion_checks = ALL_STORY_COMPLETION_CHECKS.copy()
+        self.remaining_story_completion_checks = _ALL_STORY_COMPLETION_CHECKS.copy()
         self.remaining_bonus_area_completion_locations = {}
 
     @subscribe_event
@@ -38,31 +49,32 @@ class BonusAreaCompletionChecker(ClientComponent):
         else:
             bonus_area_completion_locations = {}
             for bonus_name in event.slot_data["enabled_bonuses"]:
-                bonus_area = BONUS_NAME_TO_BONUS_AREA[bonus_name]
-                loc_ids = [LOCATION_NAME_TO_ID[f"Level Completion - Unlock {character}"]
-                           for character in bonus_area.story_characters]
+                # TODO: Put area IDs into slot data instead.
+                bonus_area = next(area for area in BONUS_ROOM_BONUSES if area.readable_name == bonus_name)
+                loc_ids = [LOCATION_NAME_TO_ID[character.get_level_completion_unlock_location_name()]
+                           for character in bonus_area.get_story_characters()]
                 server_locations = event.context.server_locations
-                bonus_area_completion_locations[bonus_area.area_id] = list(filter(server_locations.__contains__, loc_ids))
+                bonus_area_completion_locations[bonus_area] = list(filter(server_locations.__contains__, loc_ids))
             self.remaining_bonus_area_completion_locations = bonus_area_completion_locations
 
     @staticmethod
     def update_from_datastorage(ctx: TCSContext, area_ids: Iterable[AreaId]):
-        debug_logger.info("Updating Bonus Completion area_ids from datastorage: %s", area_ids)
+        _DEBUG_LOGGER.info("Updating Bonus Completion area_ids from datastorage: %s", area_ids)
         for area_id in area_ids:
-            _ap_id, address = ALL_STORY_COMPLETION_CHECKS[area_id]
+            _ap_id, address = _ALL_STORY_COMPLETION_CHECKS[Area(area_id)]
             ctx.write_byte(address, 1)
 
     async def check_completion(self, ctx: TCSContext, new_location_checks: list[int]):
         # As location checks get sent, the remaining bytes check to gets reduced.
-        updated_remaining_story_completion_checks = {}
-        write_to_datastorage_area_ids: list[AreaId] = []
-        for area_id, (ap_id, address) in self.remaining_story_completion_checks.items():
+        updated_remaining_story_completion_checks: dict[Area, tuple[ApLocationId, MemoryAddress]] = {}
+        write_to_datastorage_areas: list[Area] = []
+        for area, (ap_id, address) in self.remaining_story_completion_checks.items():
             # Memory reads are assumed to be the slowest part
             if not ctx.is_location_unchecked(ap_id):
                 # By skipping the location, it will not be added to the updated dictionary, so will not be checked in
                 # the future.
                 if ctx.is_location_sendable(ap_id):
-                    write_to_datastorage_area_ids.append(area_id)
+                    write_to_datastorage_areas.append(area)
                     # Tell the goal manager it should update for newly completed bonuses.
                     ctx.goal_manager.tag_for_update("area")
                 continue
@@ -73,21 +85,21 @@ class BonusAreaCompletionChecker(ClientComponent):
                 new_location_checks.append(ap_id)
             # Even if the location is being sent, it is still added to the updated dictionary in case the client loses
             # connection from the server.
-            updated_remaining_story_completion_checks[area_id] = (ap_id, address)
+            updated_remaining_story_completion_checks[area] = (ap_id, address)
 
         updated_remaining_bonus_area_completion_locations = {}
-        for area_id, ap_ids in self.remaining_bonus_area_completion_locations.items():
-            area_completed_or_disabled = area_id not in updated_remaining_story_completion_checks
+        for area, ap_ids in self.remaining_bonus_area_completion_locations.items():
+            area_completed_or_disabled = area not in updated_remaining_story_completion_checks
             if area_completed_or_disabled:
-                # Send the locations to AP, and skip adding the `area_id` key into
+                # Send the locations to AP, and skip adding the `area` key into
                 # `updated_remaining_bonus_area_completion_locations`.
                 # It does not matter if the locations do not exist, they will get filtered out before being sent to the
                 # server.
                 new_location_checks.extend(ap_ids)
             else:
-                updated_remaining_bonus_area_completion_locations[area_id] = ap_ids
+                updated_remaining_bonus_area_completion_locations[area] = ap_ids
 
         self.remaining_story_completion_checks = updated_remaining_story_completion_checks
         self.remaining_bonus_area_completion_locations = updated_remaining_bonus_area_completion_locations
-        ctx.update_datastorage_bonuses_completion(write_to_datastorage_area_ids)
+        ctx.update_datastorage_bonuses_completion(write_to_datastorage_areas)
 
