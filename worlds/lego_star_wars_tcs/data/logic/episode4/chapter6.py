fbba502ff79c32b62aef549dc13f1e0e44e1524e
Thomas Barlow <github@mysterymayhem.co.uk>
2026-06-28 01:42:29 +0100

    Add 4-6 individual minikits logic

--- worlds/lego_star_wars_tcs/data/logic/episode4/chapter6.py
+++ worlds/lego_star_wars_tcs/data/logic/episode4/chapter6.py
@@ -1,17 +1,160 @@
-from ..types import make_legacy_chapter, LegacyMinikitData
+from rule_builder.rules import False_, True_
 
-REBEL_ATTACK = make_legacy_chapter(
-    short_name="4-6",
-    minikits={
-        "First TIE Gate Minikit": LegacyMinikitData.new("deathstarbattle_a", "m_pup3"),
-        "First Spinner Minikit": LegacyMinikitData.new("deathstarbattle_a", "m_pup1"),
-        "First Hidden Alcove Minikit": LegacyMinikitData.new("deathstarbattle_a", "m_pup2"),
-        "Second Spinner Minikit By Second TIE Gate": LegacyMinikitData.new("deathstarbattle_b", "m_pup4"),
-        "Second TIE Gate Green Tiles Minikit": LegacyMinikitData.new("deathstarbattle_b", "m_pup5"),
-        "Second Hidden Alcove Minikit": LegacyMinikitData.new("deathstarbattle_b", "m_pup6"),
-        "Third TIE Gate Behind Force Field Minikit": LegacyMinikitData.new("deathstarbattle_b", "m_pup7"),
-        "Third Spinner Minikit": LegacyMinikitData.new("deathstarbattle_c", "m_pup8"),
-        "Third Alcove Minikit Before Trench Run": LegacyMinikitData.new("deathstarbattle_c", "m_pup9"),
-        "Fourth Spinner Minikit By Exhaust Port": LegacyMinikitData.new("deathstarbattle_d", "m_pup10"),
+from ..option_filters import logic_options
+from ..rules import (
+    HasAbility,
+    HasAllAbilities,
+)
+from ..types import minikit_data, ExitData, Chapter, LocationData
+
+from ...areas import Area
+from ...extras import Extra
+from ...levels import Level
+
+from ....character_ability import *
+
+
+# Maybe this could be in Moderate logic, but it kind of sucks due to RNG and how long it can take, so it's going in Hard
+# logic.
+_CAN_DEFLECT_BOLTS_TO_DESTROY_THINGS = logic_options(
+    base=False_(),
+    hard=Extra.DEFLECT_BOLTS.has(),
+)
+
+REBEL_ATTACK = Chapter(
+    area=Area.DEATHSTARBATTLE,
+    start_region="Spawn",
+    extra_chapter_entrance_rules=HasAbility(IS_A_VEHICLE),
+    regions={
+        "Spawn": (
+            ExitData(
+                "After Second Force Field",
+                logic_options(
+                    # Expect a blaster vehicle beyond this point.
+                    base=HasAbility(VEHICLE_BLASTER),
+                    # Just shoot a torpedo at the wall after lowering the force field.
+                    normal=True_(),
+                ),
+                new_level=Level.DEATHSTARBATTLE_B,
+            ),
+        ),
+        "After Second Force Field": (
+            ExitData(
+                "After Third Force Field",
+                # Base logic expects a blaster vehicle, and normal logic is expected to shoot these walls with
+                # torpedoes.
+                # todo: Maybe Normal logic should expect a blaster vehicle at some point?
+                #  Or normal logic could expect either a Blaster Vehicle or one out of Invincibility, Deflect Bolts,
+                #  Regenerate Hearts, for survivability purposes.
+                new_level=Level.DEATHSTARBATTLE_C,
+            ),
+        ),
+        "After Third Force Field": (
+            ExitData(
+                "Trench Run, and Exhaust Port",
+                new_level=Level.DEATHSTARBATTLE_D,
+            ),
+        ),
+        "Trench Run, and Exhaust Port": (
+            ExitData(
+                "Chapter Completion",
+                logic_options(
+                    base=True_(),
+                    # The turrets have to be destroyed to lower the force field over the exhaust port.
+                    normal=HasAbility(VEHICLE_BLASTER),
+                    # It is pretty easy to hover directly over a turret to use Deflect Bolts to destroy that turret.
+                    moderate=HasAbility(VEHICLE_BLASTER) | Extra.DEFLECT_BOLTS.has(),
+                )
+            ),
+        ),
     },
+    minikits={
+        "First Spinner Minikit": minikit_data(
+            "Spawn",
+            HasAbility(VEHICLE_BLASTER) | _CAN_DEFLECT_BOLTS_TO_DESTROY_THINGS,
+            pickup_name="m_pup1",
+        ),
+        "First Hidden Alcove Minikit": minikit_data(
+            "Spawn",
+            HasAbility(VEHICLE_BLASTER) | _CAN_DEFLECT_BOLTS_TO_DESTROY_THINGS,
+            pickup_name="m_pup2",
+        ),
+        "First TIE Gate Minikit": minikit_data(
+            "Spawn",
+            # The typical TIE gate skip of dropping in P2 at the top of P1's loop doesn't seem to work here. There are
+            # out-of-bounds tricks in this level (seems to require Clone Arcfighter), but I could not get them to work.
+            HasAbility(VEHICLE_TIE),
+            pickup_name="m_pup3",
+        ),
+        "Second Spinner Minikit By Second TIE Gate": minikit_data(
+            "After Second Force Field",
+            logic_options(
+                # Base logic expects a VEHICLE_BLASTER to get here.
+                base=True_(),
+                # Normal logic can get here by shooting a torpedo at the wall.
+                normal=HasAbility(VEHICLE_BLASTER) | _CAN_DEFLECT_BOLTS_TO_DESTROY_THINGS,
+            ),
+            pickup_name="m_pup4",
+        ),
+        "Second TIE Gate Green Tiles Minikit": minikit_data(
+            "After Second Force Field",
+            HasAbility(VEHICLE_TIE),
+            pickup_name="m_pup5",
+        ),
+        "Second Hidden Alcove Minikit": minikit_data(
+            "After Second Force Field",
+            logic_options(
+                # Base logic expects a VEHICLE_BLASTER to get here.
+                base=True_(),
+                # Expect VEHICLE_BLASTER for Normal logic.
+                normal=HasAbility(VEHICLE_BLASTER),
+                # Just use torpedoes instead.
+                moderate=True_(),
+            ),
+            pickup_name="m_pup6",
+        ),
+        "Third TIE Gate Behind Force Field Minikit": minikit_data(
+            "After Second Force Field",
+            # The minikit is not interactable until the force field has been taken down with bombs.
+            HasAllAbilities(VEHICLE_TIE | VEHICLE_TOW),
+            pickup_name="m_pup7",
+        ),
+        "Third Spinner Minikit": minikit_data(
+            "After Third Force Field",
+            logic_options(
+                # Base logic expects a VEHICLE_BLASTER to get here.
+                base=True_(),
+                # Normal logic can get here by shooting a torpedo at the wall.
+                normal=HasAbility(VEHICLE_BLASTER) | _CAN_DEFLECT_BOLTS_TO_DESTROY_THINGS,
+            ),
+            pickup_name="m_pup8",
+        ),
+        "Third Alcove Minikit Before Trench Run": minikit_data(
+            "After Third Force Field",
+            logic_options(
+                # Base logic expects a VEHICLE_BLASTER to get here.
+                base=True_(),
+                # Unlike the previous alcove, torpedoes seem to be extremely difficult to use to access this minikit,
+                # and I couldn't get Deflect Bolts to work, so a blaster vehicle is always expected. It should be noted
+                # that using Torpedoes is possible, so maybe Hard logic could logically allow Infinite Torpedos here,
+                # but it's still extremely difficult to get the torpedoes to hit the objects and gain access to the
+                # minikit.
+                normal=HasAbility(VEHICLE_BLASTER),
+            ),
+            pickup_name="m_pup9",
+        ),
+        "Fourth Spinner Minikit By Exhaust Port": minikit_data(
+            "Trench Run, and Exhaust Port",
+            logic_options(
+                base=True_(),
+                # There are no respawning enemy TIE fighters here, and the turbolaser bolts seem to be difficult, or
+                # maybe impossible, to deflect into the spinner, so VEHICLE_BLASTER is always expected.
+                normal=HasAbility(VEHICLE_BLASTER),
+            ),
+            pickup_name="m_pup10",
+        ),
+    },
+    power_brick=LocationData(
+        "Trench Run, and Exhaust Port",
+    ),
 )
