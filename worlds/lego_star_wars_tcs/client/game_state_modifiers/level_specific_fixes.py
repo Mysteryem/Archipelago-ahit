2e7cada2d1081e65a95e96190a947d4040155021
Thomas Barlow <github@mysterymayhem.co.uk>
2026-07-04 00:17:27 +0100

    Remove level-specific fix for 5-4 and Luke Skywalker (Dagobah)
    Logic now accounts for the fact that Luke Skywalker (Dagobah) cannot
    finish the level on his own.

--- worlds/lego_star_wars_tcs/client/game_state_modifiers/level_specific_fixes.py
+++ worlds/lego_star_wars_tcs/client/game_state_modifiers/level_specific_fixes.py
@@ -1,34 +1,9 @@
 from . import ClientComponent
-from ..common import StaticUint
-from ..common_addresses import IS_CHARACTER_SWAPPING_ENABLED
-from ..events import subscribe_event, OnLevelChangeEvent
 
-from ...data.characters import Character
-from ...data.levels import Level
-
-_A_VEHICLE_CHARACTER = Character.SEBULBAS_POD
-_LUKE_DAGOBAH_ID_ADDR = StaticUint(0x7f198c)
-
-
-# todo: This could be removed once new logic for Dagobah is implemented, but it could cause problems with how
-#  starting chapter ability requirements are determined.
-def _fix_luke_skywalker_dagobah(event: OnLevelChangeEvent):
-    """
-    'Fixes' `Luke Skywalker (Dagobah)` being hardcoded to be unable to lift the X-wing at the end of Dagobah, which is
-    required to complete the chapter.
-
-    The 'fix' is performed by overwriting the character ID that the game checks for when in the final level in Dagobah.
-    """
-    if event.new_level_id == Level.DAGOBAH_C and IS_CHARACTER_SWAPPING_ENABLED.get(event.context):
-        _LUKE_DAGOBAH_ID_ADDR.set(event.context, _A_VEHICLE_CHARACTER)
-    else:
-        _LUKE_DAGOBAH_ID_ADDR.set(event.context, Character.LUKE_SKYWALKER_DAGOBAH)
-
+# All Level-specific fixes have been replaced with logic that accounts for what was being fixed, so this ClientComponent
+# is currently empty.
 
 class LevelSpecificFixes(ClientComponent):
     """
     Various level-specific fixes.
     """
-    @subscribe_event
-    def on_level_change(self, event: OnLevelChangeEvent):
-        _fix_luke_skywalker_dagobah(event)
