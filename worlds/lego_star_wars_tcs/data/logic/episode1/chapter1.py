37d18c58ac47d3c48241734cfee752cc8944617a
Thomas Barlow <github@mysterymayhem.co.uk>
2026-07-30 16:03:18 +0100

    Add additional 1-1 Hard logic

--- worlds/lego_star_wars_tcs/data/logic/episode1/chapter1.py
+++ worlds/lego_star_wars_tcs/data/logic/episode1/chapter1.py
@@ -1,7 +1,8 @@
-from rule_builder.rules import And, True_
+from rule_builder.rules import And, True_, Or
+from ..macros import CAN_YODA_CLIP
 
 from ..option_filters import logic_options
-from ..rules import HasAbility, HasAllAbilities, HasAnyAbilities
+from ..rules import HasAbility, HasAllAbilities, HasAnyAbilities, HasAnyCharacterExcept
 from ..types import minikit_data, ExitData, Chapter, LocationData
 
 from ...areas import Area
@@ -31,20 +32,47 @@ NEGOTIATIONS = Chapter(
         R_MAIN_CORRIDOR: (
             ExitData(
                 R_SINGLE_FORCE_FIELD_ROOM,
-                HasAbility(PROTOCOL_PANEL),
+                logic_options(
+                    base=HasAbility(PROTOCOL_PANEL),
+                    # Yoda Clip is possible here, but the way the collision is laid out makes it more complicated than
+                    # just standing in front of the door and clipping over the ceiling directly above the door.
+                    # The collision above the door has two layers, making a Yoda clip not possible because you can only
+                    # clip through one layer, though maybe Yoda Clipping with a very short character could fit between
+                    # the two ceilings.
+                    # To Yoda Clip here, you need to stand further back from the door so that the arch's ceiling is not
+                    # above you, then, you need to Yoda Clip with good height, to get over the collision wall of the top
+                    # of the arch.
+                    # If you get in here with only a single character through a Yoda Clip, and you drop in P2 to
+                    # activate the buttons. This can make leaving the room more difficult if you don't have a
+                    # triple-high-jump character (there's no ceiling within the room), but you can stand on the box
+                    # below all the Blue Studs for extra triple jump height (remember that triple jumps with Yoda get
+                    # extra height, so start the triple jump as Yoda, then switch).
+                    hard=HasAbility(PROTOCOL_PANEL) | CAN_YODA_CLIP,
+                ),
             ),
             ExitData(
                 R_HIGH_FORCE_FIELDS_ROOM,
-                HasAbility(PROTOCOL_PANEL),
+                logic_options(
+                    base=HasAbility(PROTOCOL_PANEL),
+                    # Same door collision as the single force field room.
+                    hard=HasAbility(PROTOCOL_PANEL) | CAN_YODA_CLIP,
+                )
             ),
             ExitData(
                 R_VULTURE_DROID_ROOM,
+                # I am aware of a Yoda Clip for this door, but you tend to get stuck behind the door, or hit the
+                # backwards transition instead of the forwards transition, so I don't know how the Yoda Clip for this
+                # door is supposed to work.
                 HasAbility(ASTROMECH_PANEL),
                 new_level=Level.NEGOTIATIONS_B,
             ),
             ExitData(
                 R_SMALL_ROOM_BETWEEN_MAIN_CORRIDOR_AND_ROOM_BEFORE_HANGAR,
-                HasAbility(PROTOCOL_PANEL),
+                logic_options(
+                    base=HasAbility(PROTOCOL_PANEL),
+                    # Same door collision as the single force field room.
+                    hard=HasAbility(PROTOCOL_PANEL) | CAN_YODA_CLIP,
+                )
             ),
         ),
         R_SINGLE_FORCE_FIELD_ROOM: (),
@@ -53,12 +81,18 @@ NEGOTIATIONS = Chapter(
         R_SMALL_ROOM_BETWEEN_MAIN_CORRIDOR_AND_ROOM_BEFORE_HANGAR: (
             ExitData(
                 R_ROOM_BEFORE_HANGAR,
-                er_rule=HasAllAbilities(JEDI | CAN_BUILD_BRICKS | PROTOCOL_PANEL),
+                # If you can get to this room, you can use this entrance.
+                True_(),
+                er_rule=logic_options(
+                    base=HasAllAbilities(JEDI | CAN_BUILD_BRICKS | PROTOCOL_PANEL),
+                    hard=HasAllAbilities(JEDI | CAN_BUILD_BRICKS | PROTOCOL_PANEL) | CAN_YODA_CLIP,
+                ),
             ),
         ),
         R_ROOM_BEFORE_HANGAR: (
             ExitData(
                 R_MAIN_HANGAR,
+                True_(),
                 er_rule=HasAbility(JEDI),
                 new_level=Level.NEGOTIATIONS_C,
             ),
@@ -66,7 +100,60 @@ NEGOTIATIONS = Chapter(
         R_MAIN_HANGAR: (
             ExitData(
                 R_POWER_BRICK_ROOM,
-                HasAbility(ASTROMECH_PANEL),
+                logic_options(
+                    base=HasAbility(ASTROMECH_PANEL),
+                    # There is no ceiling here, and you can just barely triple-high-jump to get over the collision wall.
+                    # Note that triple-high-jump and then swap to Jar Jar/Tarpals/Bodyguard is impossible due to their
+                    # higher gravity, it is just barely possible to swap to them and then immediately swap back, but,
+                    # for logic simplicity, those characters will be excluded.
+                    # Once over the wall, be careful not to hit the backwards transition. You can either walk backwards
+                    # to hit the forwards transition, or triple jump over the backwards transition and into the Power
+                    # Brick room directly.
+                    # If you're swapping
+                    hard=Or(
+                        HasAbility(ASTROMECH_PANEL),
+                        And(
+                            Character.GENERAL_GRIEVOUS.has(),
+                            HasAnyCharacterExcept(
+                                # Obviously need to exclude Grievous himself.
+                                Character.GENERAL_GRIEVOUS,
+                                # High gravity (-7.00).
+                                Character.JAR_JAR_BINKS,
+                                Character.CAPTAIN_TARPALS,
+                                # Swapping from Grievous -> Bodyguard is impossible.
+                                # Swapping from Grievous -> Bodyguard -> Grievous is *barely* possible and probably too
+                                # difficult for Hard logic.
+                                # But if you have Bodyguard, it is much easier to swap from Bodyguard -> Grievous to get
+                                # enough height, so there is no need to exclude Bodyguard here.
+                                # Character.GRIEVOUS_BODYGUARD,
+                                # Don't need to exclude Yoda's because you can swap back to Grievous and still get
+                                # enough height because Grievous' gravity is the standard -6.00.
+                            ),
+                        ),
+                        And(
+                            Character.GRIEVOUS_BODYGUARD.has(),
+                            HasAnyCharacterExcept(
+                                # Obviously need to exclude Bodyguard itself.
+                                Character.GRIEVOUS_BODYGUARD,
+                                # High gravity (-7.00).
+                                Character.JAR_JAR_BINKS,
+                                Character.CAPTAIN_TARPALS,
+                                # Don't need to exclude Grievous because Grievous has -6.00 gravity, so starting the
+                                # triple-high-jump as Bodyguard and then swapping to Grievous after slamming is enough
+                                # to get the required height.
+                                # Character.GENERAL_GRIEVOUS
+                                # Yoda's have to be excluded too because if you intend to triple-high-jump with a swap
+                                # to a Yoda, you need to swap back to the original character, Bodyguard in this case,
+                                # because Yodas have a height offset in their collision, but Bodyguard's gravity is too
+                                # high to make the jump if you swap back to it.
+                                Character.YODA,
+                                Character.YODA_GHOST,
+                            ),
+                        ),
+                    ),
+                    # With a Yoda Clip, it is super easy to get out-of-bounds from where the far left Battle Droid
+                    # stands, and then you can walk under the level to get into the Power Brick room.
+                ),
                 er_rule=And(
                     HasAbility(ASTROMECH_PANEL),
                     logic_options(
@@ -110,26 +197,39 @@ NEGOTIATIONS = Chapter(
         ),
         "Force Field With Battle Droids Minikit": minikit_data(
             R_SINGLE_FORCE_FIELD_ROOM,
+            # Yoda Grab does not appear to get enough distance, so you'll need to drop in P2 if you Yoda Clipped into
+            # the room.
             pickup_name="m_pup3",
         ),
         "Minikit Behind Left High Force Field": minikit_data(
             R_HIGH_FORCE_FIELDS_ROOM,
-            HasAbility(SHORTIE),
+            logic_options(
+                base=HasAbility(PROTOCOL_PANEL),
+                # Allow Yoda Clip to activate the button.
+                # You can also Yoda Grab the minikit through the force field.
+                hard=HasAbility(PROTOCOL_PANEL) | CAN_YODA_CLIP,
+            ),
             pickup_name="m_pup5",
         ),
         "Minikit Behind Right High Force Field": minikit_data(
             R_HIGH_FORCE_FIELDS_ROOM,
-            HasAbility(SHORTIE),
-            er_rule=And(
-                # Needed to disable the force fields.
-                HasAbility(SHORTIE),
-                logic_options(
-                    # Stack 3 boxes, double jump on top, then double jump to the Minikit.
-                    base=HasAbility(JEDI),
-                    # Alternatively, high jump up to the minikit, either holding the control stick in the
-                    # direction of the elevated area to prevent sliding off, or by standing on
-                    normal=HasAnyAbilities(JEDI | HIGH_JUMP),
-                    # Moderate (simpler): Triple jump up to the minikit.
+            logic_options(
+                base=HasAbility(PROTOCOL_PANEL),
+                # Allow Yoda Clip to activate the button.
+                # You can also Yoda Grab the minikit through the force field.
+                hard=HasAbility(PROTOCOL_PANEL) | CAN_YODA_CLIP,
+            ),
+            er_rule=logic_options(
+                # Disable the force field with SHORTIE, stack 3 boxes, double jump on top, then double jump to the
+                # Minikit.
+                base=HasAllAbilities(SHORTIE | JEDI),
+                # Alternatively, high jump up to the minikit, either holding the control stick in the
+                # direction of the elevated area to prevent sliding off, or by standing on
+                normal=HasAbility(SHORTIE) & HasAnyAbilities(JEDI | HIGH_JUMP),
+                # Moderate (simpler): Triple jump up to the minikit.
+                hard=Or(
+                    HasAbility(SHORTIE) & HasAnyAbilities(JEDI | HIGH_JUMP),
+                    CAN_YODA_CLIP,
                 ),
             ),
             pickup_name="pup2",
